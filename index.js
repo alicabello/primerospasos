042357ceeb4fd2ff6fc3568b7c80111ae7e2251a
Ali Cabello <alicabelloc@gmail.com>
2022-06-30 07:46:58 -0400

    primer ejericio

---
+++ index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 5; i++) {
+
+    let edadDelUsuario = parseInt(prompt("¿Cuantos años tieness?"));
+
+    if (edadDelUsuario >= 18) {
+        alert("Bienvenido");
+
+    } else {
+        alert("Eres menor de edad");
+    }
+}
